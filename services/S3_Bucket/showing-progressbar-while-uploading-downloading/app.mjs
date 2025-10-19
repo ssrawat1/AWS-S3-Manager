aae4c31c2259171fb15b0bc9949a04d0fb11fab4
ssrawat1 <ssr911999@gmail.com>
2025-10-19 12:08:21 +0530

    Showing the progress bar while uploading the file on AWS S3 bucket

---
+++ services/S3_Bucket/showing-progressbar-while-uploading-downloading/app.mjs
@@ -0,0 +1,29 @@
+import { S3Client } from '@aws-sdk/client-s3';
+import { Upload } from '@aws-sdk/lib-storage';
+import { createReadStream } from 'node:fs';
+
+const s3Client = new S3Client({});
+
+const readStream = createReadStream('C:\\Users\\ss402\\Pictures\\Camera Roll\\coding.mp4');
+
+try {
+  const s3Uploads = new Upload({
+    client: s3Client,
+    params: {
+      Bucket: 'sanjay-pb-03',
+      Key: 'videos/coding.mp4',
+      Body: readStream,
+      ContentType: 'video/mp4',
+    },
+    // queueSize: 2,
+  });
+
+  s3Uploads.on('httpUploadProgress', ({ loaded, total }) => {
+    process.stdout.write(`\r Downloaded: ${((loaded / total) * 100).toFixed(2)} %`);
+  });
+
+  const res = await s3Uploads.done();
+  console.log('Response:', res);
+} catch (error) {
+  console.log(error);
+}
