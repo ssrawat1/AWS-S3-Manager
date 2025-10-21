27701466ff9fa138c357c9012ccf1d09e9c90724
ssrawat1 <ssr911999@gmail.com>
2025-10-21 13:48:50 +0530

    Create Signed Url using aws cloudFront signer

---
+++ services/S3_Bucket/accessing-private-bucket-objects-using-cloudFront/app.mjs
@@ -0,0 +1,25 @@
+/* Commands to create private and public keys.
+Private Key: openssl genrsa -out private_key.pem 2048
+Public Key: openssl rsa -in private_key.pem -pubout -out public_key.pem */
+
+import { getSignedUrl } from '@aws-sdk/cloudfront-signer';
+import { readFile } from 'node:fs/promises';
+
+const cloudFrontDistributionDomain = process.env.CLOUD_FRONT_DISTRIBUTION_DOMAIN;
+
+const s3ObjectKey =process.env.S3_OBJECT_KEY;
+
+const url = `${cloudFrontDistributionDomain}/${s3ObjectKey}`;
+
+const privateKey = await readFile('./private_key.pem', 'utf-8');
+
+const keyPairId = process.env.CLOUD_FRONT_KEY_PAIR_ID;
+
+const signedUrl = getSignedUrl({
+  url,
+  keyPairId,
+  dateLessThan: '2025-10-22',
+  privateKey,
+});
+
+console.log(signedUrl);
